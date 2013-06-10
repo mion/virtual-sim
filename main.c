cd0cff5496646f90e338a29a18ae39b6949d5993
Gabriel Vieira <gluisvieira@gmail.com>
2013-06-10 00:42:05 -0300

    Load memory access log file.

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 #include <string.h>
 
 #define ARGV_MAX 101
+#define MEM_ACCESS_MAX 2000
+
+struct MemAccess
+{
+    char rw;
+    unsigned addr;
+};
+
+typedef struct MemAccess MemAccess;
 
 /* Retorna 2 elevado à n. */
 int pow2(int n) {
@@ -15,8 +24,7 @@ void parse_args(int argc,
                char *alg, 
                char *filename_in, 
                int *p_size_kb, 
-               int *phys_mem_kb)
-{
+               int *phys_mem_kb) {
     if (argc != 5) {
         printf("ERRO: numero de parametros incorreto.\nRecebidos %d, esperava 5.\n", argc);
         exit(1);
@@ -28,17 +36,59 @@ void parse_args(int argc,
     }
 }
 
+int MemAccess_load(FILE *fp, MemAccess *mem_access) {
+    int i = 0;
+    unsigned addr;
+    char rw;
+
+    while(fscanf(fp, "%x %c ", &addr, &rw) == 2) {
+        if (i >= MEM_ACCESS_MAX) {
+            printf("ERROR: ultrapassado numero maximo de linhas no arquivo de log.\n");
+            exit(1);
+        }
+        mem_access[i].addr = addr;
+        mem_access[i].rw = rw;
+        i++;
+    }
+
+    return i;
+}
+
+void MemAccess_print(MemAccess mem_access) {
+    printf("0x%08x - %c\n", mem_access.addr, mem_access.rw);
+}
+
+void MemAccess_print_all(MemAccess *mem_access, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        MemAccess_print(mem_access[i]);
+    }
+}
+
 int main(int argc, char *argv[]) {
     char alg[ARGV_MAX], filename_in[ARGV_MAX];
     int p_size_kb, phys_mem_kb;
+    FILE *fp;
+    MemAccess mem_access[MEM_ACCESS_MAX];
+    int mem_access_size;
 
     parse_args(argc, argv, alg, filename_in, &p_size_kb, &phys_mem_kb);
+
+    fp = (FILE *) fopen(filename_in, "r");
+    if (!fp) {
+        printf("ERRO: nao foi possivel abrir o arquivo '%s'.\n", filename_in);
+        exit(1);
+    }
+    mem_access_size = MemAccess_load(fp, mem_access);
+    fclose(fp);
 
     printf("Executando o simulador...\n");
     printf("Arquivo de entrada: %s\n", filename_in);
     printf("Tamanho da memoria: %d\n", phys_mem_kb);
     printf("Tamanho das paginas: %d\n", p_size_kb);
     printf("Alg de substituicao: %s\n", alg);
+
+    MemAccess_print_all(mem_access, mem_access_size);
 
     return 0;
 }
