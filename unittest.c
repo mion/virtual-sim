ea8298c388ca277c19294945fc4ed8d766497003
Gabriel Vieira <gluisvieira@gmail.com>
2013-06-23 20:51:08 -0300

    Fix test file.

--- unittest.c
+++ unittest.c
@@ -1,43 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
 
 #include "simulator.h"
 
-#define ARGV_MAX 101
-
-void test(const char *msg, int expr) {
-    printf("* TEST: %s\n", msg); 
-    assert(expr);
-    printf("==> Success!\n\n");
-}
-
-void load(char *filename_in, Simulator *sim) {
-    FILE *fp = (FILE *) fopen(filename_in, "r");
-    if (!fp) {
-        printf("ERRO: nao foi possivel abrir o arquivo '%s'.\n", filename_in);
-        exit(1);
-    }
-    SimulatorLoad(fp, sim);
-    fclose(fp);
-}
-
 int main(int argc, char const *argv[]) {
-    Simulator sim;
+    Options opts;
+    Simulator * sim;
 
-    printf("===== UNIT TESTS ====\n\n");
+    strcpy(opts.filename, "file.log");
+    strcpy(opts.algo, "RND");
+    opts.p_size_kb = 4;
+    opts.phys_mem_kb = 16;
+    opts.debug_mode = 0;
 
-    /* Carrega o simulador com o arquivo de entrada. */
-    load("file.log", &sim);
+    OptionsPrint(opts);
 
-    sim.p_size_kb = 4;
-    sim.phys_mem_kb = 16;
-    sim.debug_mode = 0;
-
-    test("Loaded input file", SimulatorLength(&sim) == 7);
-
-    SimulatorRun(&sim, 0);
-    SimulatorPrintResults(&sim);
+    sim = SimulatorInit(opts);
+    SimulatorRun(sim);
+    SimulatorPrintResult(sim);
+    SimulatorDestroy(sim);
 
     return 0;
 }
